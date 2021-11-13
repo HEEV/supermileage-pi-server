0e7be35236b3d2273b468ea4447b8df57a5b4a06
ArkinSolomon <arkin@arkinsolomon.net>
2021-11-13 10:45:59 -0500

    Test using database

--- reader.py
+++ reader.py
@@ -3,12 +3,12 @@ import serial
 # Read data from serial
 class Reader:
 
-  # Class constructor, open the serial file
-  def __init__(self, file):
+    # Class constructor, open the serial file
+    def __init__(self, file):
 
-    # Set up serial
-    self.ser = serial.Serial(file, 9600)
+        # Set up serial
+        self.ser = serial.Serial(file, 9600)
 
-  # Read a line from the serial
-  def readline(self) -> str:
-    return self.ser.readline().decode('ascii').strip()
+    # Read a line from the serial
+    def readline(self) -> str:
+        return self.ser.readline().decode('ascii').strip()
--- server.py
+++ server.py
@@ -7,12 +7,14 @@ import os
 reader = Reader(os.environ.get('PICO_DEV'))
 
 # Connect to the database
-connection = create_connection("dbfilepath")
 def create_connection(path):
-  connection = None
-  try:
-    connection = sqlite3.connect(path)
-    print("Connection to SQLite DB successful")
-  except Error as e:
-    print(e)
-  return connection
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(e)
+    return connection
+
+# Open the connnection
+connection = create_connection("data/test.db")
