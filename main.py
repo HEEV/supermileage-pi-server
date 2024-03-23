879b72c6fb24795593828e072c9dbc7442923f70
ArkinSolomon <arkin@arkinsolomon.net>
2024-03-23 12:47:10 -0400

    Got pi/cloud sync

--- main.py
+++ main.py
@@ -2,18 +2,36 @@ import asyncio
 import datetime
 import math
 from dataclasses import dataclass
-
-import aiohttp.web_runner
-# async in python is dumb idek what this does but stackoverflow said to do it and now it works
-import nest_asyncio
 import serial
 import socketio
 from aiohttp import web
+import aiohttp.web_runner
+
+# async in python is dumb idek what this does but stackoverflow said to do it and now it works
+import nest_asyncio
 
 nest_asyncio.apply()
 sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins='*')
 app = web.Application()
 sio.attach(app)
+
+sio_client = socketio.SimpleClient()
+sio_client.connect('http://judas.arkinsolomon.net')
+sio_client.emit('request_write_permission', 'squid')
+
+distance_traveled = 0
+last_update = 0
+
+
+def new_race_created():
+    global distance_traveled, last_update
+    distance_traveled = 0
+    last_update = 0
+
+
+@sio.event
+async def request_new_race(sid):
+    sio_client.emit('request_new_race')
 
 
 @dataclass
@@ -21,12 +39,13 @@ class CarData:
     time: int
     voltage: float
     speed: float
+    distance_traveled: float
 
     def to_map(self):
         return {
             "time": self.time,
             "velocity": self.speed,
-            "distanceTraveled": 0,
+            "distanceTraveled": self.distance_traveled,
             "batteryVoltage": self.voltage,
             "engineTemp": 0,
             "wind": 0,
@@ -35,13 +54,25 @@ class CarData:
 
 
 def parse_line(line: str) -> CarData:
+    global last_update, distance_traveled
     output = line.split(',')
-    timestamp = math.floor(datetime.datetime.utcnow().timestamp() * 1000)
-    return CarData(time=timestamp, voltage=float(output[0]), speed=float(output[1]))
+    utc_dt_aware = datetime.datetime.now(datetime.timezone.utc)
+    timestamp = math.floor(utc_dt_aware.timestamp() * 1000)
+
+    car_speed = float(output[1])
+    if last_update > 0:
+        delta = timestamp - last_update
+
+        # mph -> Ft/ms
+        speedFtms = 0.00146667 * car_speed
+        distance_traveled += speedFtms * delta
+    last_update = timestamp
+    # print(timestamp)
+    return CarData(time=timestamp, voltage=float(output[0]), speed=car_speed, distance_traveled=distance_traveled)
 
 
 async def main():
-    port = '/dev/tty.usbserial-14110'
+    port = '/dev/tty.usbserial-14230'
     baud_rate = 9600
     ser = serial.Serial(port, baud_rate, timeout=0.025)
 
@@ -51,18 +82,37 @@ async def main():
     await aiohttp.web.TCPSite(runner, '0.0.0.0', 8080).start()
 
     while True:
-        data = ser.readline()
-        if data == b'':
-            continue
-
-        data = str(data)[2:-5]
 
         try:
-            data = parse_line(data)
+            event = sio_client.receive(timeout=1)
+            if event is not None:
+                event_name = event[0]
+                if event_name == 'permission_granted':
+                    sio_client.emit('request_new_race')
+                elif event_name == 'new_race_created':
+                    new_race_created()
+
+            # print(f'received event: "{event[0]}" with arguments {event[1:]}')
+        except:
+            pass
+
+        try:
+            last_line = ser.readline().decode('utf-8')
+            next_line = ser.readline().decode('utf-8')
+            while next_line != '':
+                last_line = next_line
+                next_line = ser.readline().decode('utf-8')
+        except:
+            continue
+
+        try:
+            data = parse_line(last_line)
             print(data)
             await sio.emit('new_data', data.to_map())
+            sio_client.emit('write_data', data.to_map())
             await asyncio.sleep(.25)
         except:
+            print(f'Exception while parsing/sending data: {last_line}')
             pass
 
 
